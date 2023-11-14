53f6f1dfb136ad891b9cc4660a0fdb9410cc88f3
swailoong <110377606+swailoong@users.noreply.github.com>
2023-11-14 20:18:48 +0800

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,8 @@
+act1 = input ("Activity : ")
+adj = input ("Adjective : ")
+adj2 = input ("Adjective : ")
+famous_person = input ("Famous Person : ")
+
+madlib = f"{act1} is a {adj} activity to try out. It is so {adj2} that no one has ever surpassed {famous_person} in this game."
+
+print (madlib)
